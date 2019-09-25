86347086b00c7412fffcb63a4df1cb167dd20f9d
nhurtado <nhurtado@miuandes.cl>
2019-09-24 21:23:59 -0300

    added base project with Player and Player Script.

---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 30f;
+    Vector2 currentMovement;
+    void FixedUpdate()
+    {
+        ProcessInput();
+        UpdateState();
+    }
+
+    void ProcessInput()
+    {
+        currentMovement = transform.position;
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            currentMovement.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            currentMovement.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            currentMovement.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentMovement.x -= speed * Time.deltaTime;
+        }
+    }
+
+    void UpdateState()
+    {
+        transform.position = currentMovement;
+    }
+}
