5b27188afb316a7d63768e8a4d0bf1cf6b042b19
Francisco Alvarez <s2globalmail@gmail.com>
2019-10-01 22:11:25 -0300

    Walls and collision with sound added.

---
+++ Assets/ImpactManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ImpactManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            EditorApplication.Beep();
+        }
+    }
+}
