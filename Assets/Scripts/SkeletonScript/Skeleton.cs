4c4cd320dedb1a3772b546868b2eb8c55f6f3ba6
JoaquinFernandezC <jafernandez1@miuandes.cl>
2019-10-08 23:56:01 -0300

    Component Pattern applied to Skeleton.cs

---
+++ Assets/Scripts/SkeletonScript/Skeleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    private ShootingComponent shootingComp;
+    private TimingComponent timingComp;
+    public float xPosition;
+    public float yPosition;
+    public Skeleton(ShootingComponent shootingComp, TimingComponent timingComp)
+    {
+        this.shootingComp = shootingComp;
+        this.timingComp = timingComp;
+    }
+    void Start()
+    {
+        xPosition = transform.position.x;
+        yPosition = transform.position.y;
+    }
+
+}
