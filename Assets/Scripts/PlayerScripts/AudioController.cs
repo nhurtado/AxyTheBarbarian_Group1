3cde7cb18689f8f3744bbbbd537b353cad5418c5
nhurtado <nhurtado@miuandes.cl>
2019-10-16 20:18:41 -0300

    Added Audio Controller.

---
+++ Assets/Scripts/PlayerScripts/AudioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    AudioSource wallHit;
+
+    void Start()
+    {
+        wallHit = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Wall" || other.gameObject.tag == "Projectile")
+        {
+            wallHit.Play();
+        }
+    }
+}
